f5430fd13f58d2a8a9e7f89a5bd75a08289d833c
BedeschiL <louis.bedeschi@gmail.com>
2023-06-02 16:42:49 +0200

    added test for collections

--- src/test/api_test.py
+++ src/test/api_test.py
@@ -160,6 +160,85 @@ class ApiTest(unittest.TestCase):
 
         self.assertEquals(status_code, 401)
 
+
+    ####
+    def test_collections(self):
+        cf = self.read_config_file("../API/config/api_config.yaml")
+        base_url = cf.get("BASE_URL")
+        port = cf.get("PORT")
+        url = "{}:{}/example1/collections".format(base_url, port)
+        response = requests.get(url)
+        status_code = response.status_code
+        self.assertEquals(status_code, 401)
+
+    def test_collections_no_auth_no_accept(self):
+        cf = self.read_config_file("../API/config/api_config.yaml")
+        base_url = cf.get("BASE_URL")
+        port = cf.get("PORT")
+
+        url = "{}:{}/example1/collections".format(base_url, port)
+        response = requests.get(url)
+        status_code = response.status_code
+
+        self.assertEquals(status_code, 401)
+
+    def test_collections_auth_only_no_accept(self):
+        cf = self.read_config_file("../API/config/api_config.yaml")
+        base_url = cf.get("BASE_URL")
+        port = cf.get("PORT")
+        passwd = cf.get("PASSWORD")
+        user = cf.get("USER")
+
+        url = "{}:{}/example1/collections".format(base_url, port)
+        response = requests.get(url, auth=HTTPBasicAuth(user, passwd))
+        status_code = response.status_code
+
+        self.assertEquals(status_code, 406)
+
+    def test_collections_auth_accept(self):
+        cf = self.read_config_file("../API/config/api_config.yaml")
+        base_url = cf.get("BASE_URL")
+        port = cf.get("PORT")
+        passwd = cf.get("PASSWORD")
+        user = cf.get("USER")
+
+        url = "{}:{}/example1/collections".format(base_url, port)
+        response = requests.get(url, auth=HTTPBasicAuth(user, passwd),
+                                headers={'Accept': 'application/taxii+json;version=2.1'})
+        status_code = response.status_code
+
+        self.assertEquals(status_code, 200)
+
+    def test_collections_auth_accept_wrong_user_pass(self):
+        cf = self.read_config_file("../API/config/api_config.yaml")
+        base_url = cf.get("BASE_URL")
+        port = cf.get("PORT")
+        user = "blabla"
+        passwd = "blabla"
+
+        url = "{}:{}/example1/collections".format(base_url, port)
+        response = requests.get(url, auth=HTTPBasicAuth(user, passwd),
+                                headers={'Accept': 'application/taxii+json;version=2.1'})
+        status_code = response.status_code
+
+        self.assertEquals(status_code, 401)
+
+    def test_collections_auth_accept_right_user_wrong_password(self):
+        cf = self.read_config_file("../API/config/api_config.yaml")
+        base_url = cf.get("BASE_URL")
+        port = cf.get("PORT")
+        user = cf.get("USER")
+        passwd = "blabla"
+        url = "{}:{}/example1/collections".format(base_url, port)
+
+        response = requests.get(url, auth=HTTPBasicAuth(user, passwd),
+                                headers={'Accept': 'application/taxii+json;version=2.1'})
+        status_code = response.status_code
+
+        self.assertEquals(status_code, 401)
+
+
+
     def read_config_file(self, filepath):
         file_directory = os.path.dirname(os.path.abspath(__file__))
         print(file_directory)
